960c785b1853930dce2237821a7715ec31898208
Steve Hannay <hannay.steve@googlemail.com>
2024-01-28 23:45:38 +0000

    first weather icon read and populated

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ $("#search-button").on('click', function(event){
     }
     else {
         // Search for the Cities weather, Display Results and create a Button in the search "History" section 
-        performSearch(userInput)
+        performWeatherSearch(userInput)
     }
  
 })
@@ -39,12 +39,15 @@ $("#search-button").on('click', function(event){
 // ----------
 
 // Search for a Cities weather, Display Results and create a Button in the search "History" section 
-function performSearch(cityName){
-
+function performWeatherSearch(cityName){
 
     var apiURL_GeoCordinatesByLocationName = "http://api.openweathermap.org/geo/1.0/direct?"
     var apiURL_TodaysWeatherByCityName = "https://api.openweathermap.org/data/2.5/weather?"
     
+    var apiURL_5DayForecastByCityName = "https://api.openweathermap.org/data/2.5/forecast?"
+
+    
+
     var apiKey = "bacc1eea38f464a624c0b32d6374373c"   // OpenWeatherMap key
     
     var queryURL = ""
@@ -58,7 +61,7 @@ function performSearch(cityName){
     // note : other API calls are nested within this one, as we need to have the Geocordoinates before continuing
 
     queryURL = apiURL_GeoCordinatesByLocationName + "q=" + cityName + "&limit=1&appid=" + apiKey
-    console.log('Calling API : ' +  queryURL)
+    console.log('API CALL 1 : ' +  queryURL)
 
     // return data from API
     fetch(queryURL)
@@ -72,25 +75,25 @@ function performSearch(cityName){
             return // STOP
         }
 
-        // check that the data returned is valid for the selected City
-        if (data[0].name.toUpperCase() == cityName.toUpperCase()) {
-            console.log("Geocordinates found for : city = " + data[0].name + ", lat = " + cityLatitude + ", lon = " + cityLongtitude)
-        }
-        else {
-            console.log("WARNING : API returned geocoordinates for " + data[0].name)
-        }
-        console.log(data)
-        
         // get geocordinates
         cityLatitude = data[0].lat
         cityLongtitude = data[0].lon 
 
 
+        // check that the data returned is valid for the selected City
+        if (data[0].name.toUpperCase() == cityName.toUpperCase()) {
+            console.log("Geocordinates found for : city = " + data[0].name + ", lat = " + cityLatitude + ", lon = " + cityLongtitude)
+        }
+        else {
+            console.log("WARNING : API returned geocoordinates for " + data[0].name + ", lat = " + cityLatitude + ", lon = " + cityLongtitude)
+        }
+        
+
 
         // API CALL 2 - Get Todays Weather for the selected City
 
         queryURL = apiURL_TodaysWeatherByCityName + "lat=" + cityLatitude + "&lon=" + cityLongtitude + "&appid=" + apiKey
-        console.log('Calling API : ' +  queryURL)
+        console.log('API CALL 2 : ' +  queryURL)
 
         // return data from API
         fetch(queryURL)
@@ -100,14 +103,30 @@ function performSearch(cityName){
 
             console.log("Todays Weather in " + data.name + " : ")
             console.log(data)
+
+            // get todays weather
+            var iconCode = data.weather[0].icon
+
+            var iconURL = "https://openweathermap.org/img/w/"
+   
+            queryURL =  iconURL + iconCode + ".png"
             
+            console.log("image : " + queryURL)
+
+
+            // display todays weather
+
+            $('#todays-weather-icon').attr('src', queryURL);
+
+
+
         })
 
 
-/*         // API CALL 3 - Get 5 Day Forecast for the selected City
+        // API CALL 3 - Get 5 Day Forecast for the selected City
         
-        queryURL = apiURL_RequestByCityName + "lat=" + cityLatitude + "&lon=" + cityLongtitude + "&appid=" + apiKey
-        console.log('Calling API : ' +  queryURL)
+        queryURL = apiURL_5DayForecastByCityName + "lat=" + cityLatitude + "&lon=" + cityLongtitude + "&appid=" + apiKey
+        console.log('API CALL 3 : ' +  queryURL)
 
         // return data from API
         fetch(queryURL)
@@ -115,10 +134,10 @@ function performSearch(cityName){
             return response.json()
         }).then(function (data) {
 
-            console.log("5 Day Forecast for " + data.name + " : ")
+            console.log("5 Day Forecast for " + data.city.name + " : ")
             console.log(data)
             
-        }) */
+        })
 
 
 
