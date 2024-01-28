a87b1ce062ffb83db873c7aa70824b0b34f778e2
Steve Hannay <hannay.steve@googlemail.com>
2024-01-28 15:43:28 +0000

    initial event handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,41 @@
+/* 
+    Javascript for Challenge 08 
+    Developer - Steve Hannay 
+    First Created - 27th January 2024
+
+    This code handles logic for the Weather Dashboard
+
+*/
+
+
+// Define Variables and Event Listeners
+// ------------------------------------
+
+// create references to html elements
+var txtSearchText = document.querySelector("#search-input")  // reference by id
+var btnSearchButton = document.querySelector("#search-button")  // reference by id
+
+
+// Event Listeners
+
+// - Search button "click" event
+$("#search-button").on('click', function(event){
+    
+    //alert("event")
+
+    var city = $("#search-input").val()
+
+    performSearch(city)
+
+ 
+})
 
 
 
-// alert("SCRIPT")
+// MAIN Logic
+
+// Search for a Cities weather, Display Results and create a Button in the search "History" section 
+function performSearch(cityName){
+    alert("Serching for : " + cityName)
+
+}
