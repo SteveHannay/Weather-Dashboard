a6260c9a1baca4c1d1a653be4e013c2ad570302b
Steve Hannay <hannay.steve@googlemail.com>
2024-01-29 03:09:07 +0000

    todays weather brought to screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,20 +41,23 @@ $("#search-button").on('click', function(event){
 // Search for a Cities weather, Display Results and create a Button in the search "History" section 
 function performWeatherSearch(cityName){
 
+    // Declare variables and initialise
     var apiURL_GeoCordinatesByLocationName = "http://api.openweathermap.org/geo/1.0/direct?"
     var apiURL_TodaysWeatherByCityName = "https://api.openweathermap.org/data/2.5/weather?"
-    
     var apiURL_5DayForecastByCityName = "https://api.openweathermap.org/data/2.5/forecast?"
 
+    var apiKey = "bacc1eea38f464a624c0b32d6374373c"     // OpenWeatherMap key
     
+    var iconURL = "https://openweathermap.org/img/w/"   // For displaying icons in an image
 
-    var apiKey = "bacc1eea38f464a624c0b32d6374373c"   // OpenWeatherMap key
-    
     var queryURL = ""
-    var cityLatitude = "44.34" // test values
-    var cityLongtitude = "10.99"
+    var cityLatitude = "" 
+    var cityLongtitude = ""
 
-    console.log("**** NEW SEARCH FOR '" + cityName + "' WEATHER ****")
+    var todaysDateUKFormatted = dayjs().format("D/M/YYYY")
+
+
+    console.log("**** NEW SEARCH FOR WEATHER IN '" + cityName + "' ****")
 
 
     // API CALL 1 - Get Geocordinates for the selected City 
@@ -92,7 +95,7 @@ function performWeatherSearch(cityName){
 
         // API CALL 2 - Get Todays Weather for the selected City
 
-        queryURL = apiURL_TodaysWeatherByCityName + "lat=" + cityLatitude + "&lon=" + cityLongtitude + "&appid=" + apiKey
+        queryURL = apiURL_TodaysWeatherByCityName + "lat=" + cityLatitude + "&lon=" + cityLongtitude + "&units=metric&appid=" + apiKey
         console.log('API CALL 2 : ' +  queryURL)
 
         // return data from API
@@ -105,27 +108,32 @@ function performWeatherSearch(cityName){
             console.log(data)
 
             // get todays weather
-            var iconCode = data.weather[0].icon
+            var weatherDescription = data.weather[0].description
 
-            var iconURL = "https://openweathermap.org/img/w/"
-   
+            var iconCode = data.weather[0].icon
             queryURL =  iconURL + iconCode + ".png"
-            
             console.log("image : " + queryURL)
 
+            var weatherTemp = data.main.temp
+            var weatherWind = data.wind.speed
+            var weatherHumidity = data.main.humidity
 
             // display todays weather
+            $('#todays-weather-citydate').text(data.name + " (" + todaysDateUKFormatted + ") ")
 
-            $('#todays-weather-icon').attr('src', queryURL);
+            $('#todays-weather-icon').attr("src", queryURL);
+            $('#todays-weather-icon').attr("alt", weatherDescription);
 
-
+            $('#todays-weather-temp').text("Temp : " + weatherTemp + " °C")
+            $('#todays-weather-wind').text("Wind : " + weatherWind + " KPH")
+            $('#todays-weather-humidity').text("Humidity : " + weatherHumidity + " %")
 
         })
 
 
         // API CALL 3 - Get 5 Day Forecast for the selected City
         
-        queryURL = apiURL_5DayForecastByCityName + "lat=" + cityLatitude + "&lon=" + cityLongtitude + "&appid=" + apiKey
+        queryURL = apiURL_5DayForecastByCityName + "lat=" + cityLatitude + "&lon=" + cityLongtitude + "&units=metri&appid=" + apiKey
         console.log('API CALL 3 : ' +  queryURL)
 
         // return data from API
@@ -136,6 +144,21 @@ function performWeatherSearch(cityName){
 
             console.log("5 Day Forecast for " + data.city.name + " : ")
             console.log(data)
+
+            var forecastDate = dayjs() // initialise
+
+
+            // Forecast Day 1
+
+            // get next forecast date and convert to a DT code 
+
+            // get index of MIDDAY forcast for forecast date
+
+            // get forecast weather
+
+            // display forecast weather
+
+
             
         })
 
@@ -143,18 +166,5 @@ function performWeatherSearch(cityName){
 
 
     })
-
-
-
-
-
-
-
-
-
-
-    
-
-
 
 }
